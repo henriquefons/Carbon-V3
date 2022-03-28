a5c1119d0898635f957ea5cc2e127c4de4e0c36f
Henrique Fonseca <henriquefons@hotmail.com>
2022-03-28 09:47:41 -0300

    Add carbon components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="about">
     <h1>Home</h1>
+    <cv-button-set>
+      <CvButton kind="primary">button 1</CvButton>
+      <CvButton kind="secondary">button 2</CvButton>
+      <CvButton kind="danger">button 3</CvButton>
+    </cv-button-set>
   </div>
 </template>
 
 <script>
-import {} from "@/components/bundles/cv-button";
+import { CvButton } from "@/components/bundles/cv-button";
 // @ is an alias to /src
-export default {};
+export default {
+  components: {
+    CvButton,
+  },
+};
 </script>
